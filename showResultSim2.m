716c645ac014cb095c6dfba23cb43a74e6eb3643
Raise-NCCR <raise1229@gmail.com>
2024-02-15 22:55:34 +0900

    splited showResult

---
+++ showResultSim2.m
@@ -0,0 +1,170 @@
+%sim1
+load("xHistory1.mat");
+% load("mvHistory1.mat");
+
+xHis1 = xHistory;
+% mvHis1 = mvHistory;
+
+%sim2
+load("xHistory2.mat");
+% load("mvHistory2.mat");
+
+xHis2 = xHistory;
+% mvHis2 = mvHistory;
+
+Ts = 0.1;
+
+r1 = ridecomfort(Ts,xHis1);
+r2 = ridecomfort(Ts,xHis2);
+
+mean(r1)
+mean(r2)
+
+len = length(xHis2(1,:));
+
+tmp = zeros(length(xHis1(:,1)), len-length(xHis1(1,:)));
+xHis1 = [xHis1 tmp];
+tmp = zeros(1,len-length(r1(1,:)));
+r1 = [r1 tmp];
+tmp = zeros(2,len-length(r1(1,:))+1);
+
+
+time = (0:0.1:(len/10)-0.1);
+
+%従来法
+load('zhouState.mat');
+load('zhouHis.mat');
+
+zhouR = ridecomfort(Ts,zhouState(:, 1:100:end));
+
+zhouRes = zeros(10,length(time));
+
+zhouRes(:,1:61) = [zhouHistory(1,1:100:6001);
+    zhouHistory(2,1:100:6001);
+    zhouHistory(3,1:100:6001);
+    zhouHistory(4,1:100:6001);
+    zhouHistory(5,1:100:6001);
+    zhouHistory(6,1:100:6001);
+    zhouHistory(7,1:100:6001);
+    zhouHistory(8,1:100:6001);
+    zhouHistory(9,1:100:6001);
+    zhouHistory(10,1:100:6001)];
+
+figure;
+subplot(2,5,1);
+plot(time',zhouRes(1,:),'k')
+hold on
+subplot(2,5,2);
+plot(time',zhouRes(2,:),'k')
+hold on
+subplot(2,5,3);
+plot(time',zhouRes(3,:),'k')
+hold on
+subplot(2,5,4);
+plot(time',zhouRes(4,:),'k')
+hold on
+subplot(2,5,5);
+plot(time',zhouRes(5,:),'k')
+hold on
+subplot(2,5,6);
+plot(time',zhouRes(6,:),'k')
+hold on
+subplot(2,5,7);
+plot(time',zhouRes(7,:),'k')
+hold on
+subplot(2,5,8);
+plot(time',zhouRes(8,:),'k')
+hold on
+subplot(2,5,9);
+plot(time',zhouRes(9,:),'k')
+hold on
+subplot(2,5,10);
+plot(time',zhouRes(10,:),'k')
+hold on
+
+subplot(2,5,1);
+plot(time',xHis1(13,1:length(time)),'b');
+hold on
+subplot(2,5,2);
+plot(time',xHis1(1,1:length(time)),'b')
+hold on
+subplot(2,5,3);
+plot(time',xHis1(9,1:length(time)),'b')
+hold on
+subplot(2,5,4);
+plot(time',xHis1(10,1:length(time)),'b')
+hold on
+subplot(2,5,5);
+plot(time',xHis1(4,1:length(time)),'b')
+hold on
+subplot(2,5,6);
+plot(time',xHis1(3,1:length(time)),'b')
+hold on
+subplot(2,5,7);
+plot(time',r1(1:length(time)),'b')
+hold on
+subplot(2,5,8);
+plot(time',xHis1(14,1:length(time)),'b')
+hold on
+
+
+subplot(2,5,1);
+plot(time',xHis2(13,1:length(time)),'r');
+legend('従来法','提案法sim1', '提案法sim2');
+grid on
+xlabel('Time [s]');
+ylabel('xJerk [m/s^3]');
+subplot(2,5,2);
+plot(time',xHis2(1,1:length(time)),'r')
+legend('従来法','提案法sim1', '提案法sim2');
+grid on
+xlabel('Time [s]');
+ylabel('Velocity [m/s]');
+subplot(2,5,3);
+plot(time',xHis2(9,1:length(time)),'r')
+legend('従来法','提案法sim1', '提案法sim2');
+grid on
+xlabel('Time [s]');
+ylabel('Acceleration [m/s^2]');
+subplot(2,5,4);
+plot(time',xHis2(10,1:length(time)),'r')
+legend('従来法','提案法sim1', '提案法sim2');
+grid on
+xlabel('Time [s]');
+ylabel('Lateral acceleration [m/s^2]');
+subplot(2,5,5);
+plot(time',xHis2(4,1:length(time)),'r')
+legend('従来法','提案法sim1', '提案法sim2');
+grid on
+xlabel('Time [s]');
+ylabel('Yaw rate [rad/s]');
+subplot(2,5,6);
+plot(time',xHis2(3,1:length(time)),'r')
+legend('従来法','提案法sim1', '提案法sim2');
+grid on
+xlabel('Time [s]');
+ylabel('Sideslip angle [rad]');
+subplot(2,5,7);
+plot(time',r2(1:length(time)),'r')
+legend('従来法','提案法sim1', '提案法sim2');
+grid on
+xlabel('Time [s]');
+ylabel('d(t)');
+subplot(2,5,8);
+plot(time',xHis2(14,1:length(time)),'r')
+legend('従来法','提案法sim1', '提案法sim2');
+grid on
+xlabel('Time [s]');
+ylabel('yJerk [m/s^3]');
+% subplot(2,5,9);
+% plot(time',mvHis2(1,1:length(time)),'r')
+% legend('従来法','提案法');
+% grid on
+% xlabel('Time [s]');
+% ylabel('inputJerk [m/s^3]');
+% subplot(2,5,10);
+% plot(time',mvHis2(2,1:length(time)),'r')
+% legend('従来法','提案法');
+% grid on
+% xlabel('Time [s]');
+% ylabel('inputDelta [rad/s]');
